e5c87c012746a5b6c7a9b4025657979181fd9ec0
masihua123 <890721msh>
2016-05-06 18:29:09 -0600

    First Version:splash+guide(finished)

--- WisdowmNewsClient/src/main/java/com/example/sihua/wisdomnews/GuideActivity.java
+++ WisdowmNewsClient/src/main/java/com/example/sihua/wisdomnews/GuideActivity.java
@@ -1,5 +1,6 @@
 package com.example.sihua.wisdomnews;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.print.PageRange;
 import android.support.v4.view.PagerAdapter;
@@ -8,9 +9,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
@@ -26,13 +29,23 @@ public class GuideActivity extends Activity {
     private LinearLayout ll_page;
     @ViewInject(R.id.btn_start)
     private Button btn_start;
-
+    @ViewInject(R.id.rl_page)
+    private RelativeLayout rl_page;
+    @ViewInject(R.id.view_red)
+    private View view_red;
     private int[] images=new int[]{R.mipmap.guide_1,R.mipmap.guide_2,R.mipmap.guide_3};
     List<ImageView> imagelist;
+    private int distance;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
+        initListener();
+    }
+
+    private void initListener() {
+        vp_content.setOnPageChangeListener(new MyPageChangeListener());
     }
 
     private void initView() {
@@ -45,6 +58,28 @@ public class GuideActivity extends Activity {
             iv.setBackgroundResource(images[i]);
             imagelist.add(iv);
         }
+        for (int i = 0; i < images.length; i++) {
+            View view=new ImageView(this);
+            view.setBackgroundResource(R.drawable.shape_point_gray);
+            LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(50,50);
+            if(i>0){
+                params.leftMargin=40;
+            }
+            view.setLayoutParams(params);
+            ll_page.addView(view);
+        }
+        // 此时拿不到，因为还没有画在界面上
+        ViewTreeObserver viewTreeObserver = ll_page.getViewTreeObserver();//获取视图树
+        //获取视图树对界面的监听
+        viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            //当layout运行结束后回调此方法
+            @Override
+            public void onGlobalLayout() {
+                distance = ll_page.getChildAt(1).getLeft()-ll_page.getChildAt(0).getLeft();
+                ll_page.getViewTreeObserver().removeOnGlobalLayoutListener(this);//各个版本没问题
+            }
+        });
         //set adapter
         vp_content.setAdapter(new MyAdapter());
     }
@@ -67,12 +102,44 @@ public class GuideActivity extends Activity {
         public Object instantiateItem(ViewGroup container, int position) {
             currentView = imagelist.get(position);
             container.addView(currentView);
+            //change point color
+
             return currentView;
         }
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
             container.removeView(imagelist.get(position));
+        }
+    }
+
+    private class MyPageChangeListener implements ViewPager.OnPageChangeListener {
+
+        //滑动事件
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            //百分比会归零
+            float len=distance*positionOffset+position*distance;
+            //获取布局参数
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view_red.getLayoutParams();
+            layoutParams.leftMargin=(int)len;//设置左边距
+            view_red.setLayoutParams(layoutParams);
+        }
+
+        //页面被选中
+        @Override
+        public void onPageSelected(int position) {
+            if(position==images.length-1){
+                btn_start.setVisibility(View.VISIBLE);
+            }else{
+                btn_start.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        //滑动状态发生变化
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
         }
     }
 }
